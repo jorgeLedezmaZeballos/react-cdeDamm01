d31056b0e7f383189948e9a9a040a0b615cac911
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-10-20 11:53:52 -0400

    componente tabla adherido como Prod.jsx mas su css

---
+++ src/Prod.jsx
@@ -0,0 +1,21 @@
+xport function Prod(){
+    return <table>
+            <tr>
+                <th>nombre</th>
+                <th>apellido</th>
+            </tr>
+            <tr>
+                <td>jorge</td>
+                <td>ldzma</td>
+            </tr>
+            <tr>
+                <td>selina</td>
+                <td>perez</td>
+            </tr>
+        </table>
+    
+}
+
+export function Saludo(){
+    return <h1>hola mundo</h1>
+}
