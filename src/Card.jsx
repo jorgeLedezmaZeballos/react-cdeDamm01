0a0036b5c9e26081482acce6bd4d9fdc197f9273
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-10-20 12:03:32 -0400

    incorporamos el card al main

---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+
+export function Card(){
+  return <div class= "tarjeta">
+        <button class= "boton-x">X</button>
+        <h2>Recursos</h2>
+        <p>Lo utlimo para averiguar</p>
+    </div>
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Prod, Saludo } from './Prod'
+import {Card} from './Card'
 import './assets/Prod.css'
-//import './index.css'
+import './Card.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Prod />
     <Saludo />
+	<Card />
   </>
 )
